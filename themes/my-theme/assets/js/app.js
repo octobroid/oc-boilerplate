4297954270d1cb458eb9f40729225246cdabf80c
Trias Nur Rahman <contact@triasrahman.com>
2017-08-21 19:43:20 +0700

    Tidy up app.js and set flash by default

--- themes/my-theme/assets/js/app.js
+++ themes/my-theme/assets/js/app.js
@@ -1,17 +1,30 @@
-+function($) { "use strict";
++function ($) {
+	'use strict';
 
-	$(document).ready(function() {
-
+	$(document).ready(function () {
 	    // Attach fastclick
-	    FastClick.attach(document.body);
+	    FastClick.attach(document.body)
 
 	    // Activate tooltip bootstrap
 	    $('[data-toggle=tooltip]').tooltip({
-	        container: 'body',
-	    });
+	        container: 'body'
+	    })
 
 	    // Add your own custom script
 	    // ...
 	});
+
+	$(document).on('ajaxSetup', function (event, context) {
+		// Enable AJAX handling of Flash messages on all AJAX requests
+		context.options.flash = true
+
+		context.options.handleErrorMessage = function (message) {
+			$.oc.flashMsg({ text: message, class: 'error' })
+		}
+
+		context.options.handleFlashMessage = function (message, type) {
+			$.oc.flashMsg({ text: message, class: type })
+		}
+	})
 
 }(window.jQuery);
