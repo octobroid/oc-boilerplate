89bd73e9195e187598e62b29c9e0723d7afccc96
Trias Nur Rahman <contact@triasrahman.com>
2018-01-04 12:15:10 +0700

    Add modal control AJAX

--- themes/my-theme/assets/js/app.js
+++ themes/my-theme/assets/js/app.js
@@ -27,4 +27,55 @@
 		}
 	})
 
+    /**
+	 *
+	 * Modal Control AJAX
+	 *
+	*/
+	$(document).on('click', '[data-control=modal]', function(event) {
+	    event.preventDefault()
+
+	    var $el = $(this)
+
+	    var modal = bootbox.dialog({
+	        message: '<div class="text-center"><p><i class="fa fa-spin fa-spinner fa-4x"></i></p><p class="lead">Loading...</p></div>',
+	        size: $el.data('modalSize'),
+	        className: $el.data('modalClass'),
+	        backdrop: true,
+	        onEscape: true,
+	    })
+
+        modal.attr('id', 'modal-' + new Date().valueOf() )
+
+	    var update = {}
+
+	    update[$el.data('partial')] = '#' + modal.attr('id') + ' .modal-content'
+
+	    $el.request($el.data('handler'), {
+	        update: update,
+
+	        complete: function(complete, b) {
+	            // e.preventDefault()
+	            if (! complete.responseJSON) {
+	                modal.modal('hide')
+	                bootbox.alert(complete.responseText)
+	            }
+	        },
+	        error: function(error) {}
+	    })
+
+	    return false
+	})
+
+    /**
+     * Modal Z-index Fix
+     */
+    $(document).on('show.bs.modal', '.modal', function () {
+        var zIndex = 1040 + (10 * $('.modal:visible').length);
+        $(this).css('z-index', zIndex);
+        setTimeout(function() {
+            $('.modal-backdrop').not('.modal-stack').css('z-index', zIndex - 1).addClass('modal-stack');
+        }, 0);
+    });
+
 }(window.jQuery);
